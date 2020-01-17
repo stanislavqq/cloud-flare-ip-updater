7573f98720ce48ca2c25dad0abda281652dd9757
Stanislav QQ <stanislavqq@yandex/ru>
2020-01-17 18:41:13 +0600

    Added config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'api_key'           => getenv('API_KEY'),
+    'api_email'         => getenv('API_EMAIL'),
+    'zone_id'           => getenv('ZONE_ID'),
+    'domain'            => getenv('BASE_DOMAIN'),
+    'file_cache_name'   => getenv('FILE_CACHE_IP_NAME')
+];
